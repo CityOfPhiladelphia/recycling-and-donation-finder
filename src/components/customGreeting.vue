4993bf6ae65665cb1267db770c902712a0f727c9
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-11-12 15:09:22 -0500

    first changes for vue3

--- src/components/customGreeting.vue
+++ src/components/customGreeting.vue
@@ -1,122 +1,133 @@
+<script setup>
+
+// import $config from '../main.js';
+
+// const props = defineProps({
+//   'message': {
+//     type: String,
+//     default: function() {
+//       return 'defaultMessage';
+//     },
+//   },
+// });
+  
+// const sections = ref({});
+// const subsections = ref({});
+
+// computed
+// const i18nEnabled = computed(() => {
+//   if ($config.i18n) {
+//     return true;
+//   }
+//   return false;
+// });
+
+// const calloutOptions = computed(() => {
+//   return {};
+// });
+
+// const calloutSlots = computed(() => {
+//   return {
+//     text: 'test',
+//   };
+// });
+
+// const hasError = computed(() => {
+//   return this.$store.state.geocode.status === 'error';
+// });
+
+// const errorMessage = computed(() => {
+//   const input = this.$store.state.geocode.input;
+//   return `
+//       <p>
+//         We couldn't find
+//         ${input ? '<strong>' + input + '</strong>' : 'that address'}.
+//         Are you sure everything was spelled correctly?
+//       </p>
+//       <p>
+//         Here are some examples of things you can search for:
+//       </p>
+//       <ul>
+//         <li>1234 Market St</li>
+//         <li>1001 Pine Street #201</li>
+//         <li>12th & Market</li>
+//         <li>883309050 (an OPA number with no hyphens or other characters)</li>
+//       </ul>
+//     `;
+// });
+
+</script>
+
 <template>
   <div
-    class="content"
+    class="main-greeting"
   >
-    <div class="section has-text-centered">
-      <button
-        class="button open-list-button"
-        @click="$emit('view-list')"
-        v-html="$t('app.viewList')"
-      />
+    <div class="half-data-section">
+      <div class="has-text-centered container">
+        <button
+          class="button greeting-button"
+          @click="$emit('view-list')"
+          v-html="$t('app.viewList')"
+        />
+      </div>
     </div>
 
-    <div class="section">
+    <div class="half-data-section">
+
       <h1 class="title is-2 is-spaced">
         About this finder
       </h1>
-      <p>This tool can help you find find places to donate or recycle items in Philadelphia. You can:</p>
-      <div class="app-list">
-        <ul>
+
+      <div class="half-data-section">
+        <p>This tool can help you find find places to donate or recycle items in Philadelphia. You can:</p>
+      </div>
+
+      <div class="half-data-section">
+        <ul class="bullet-list">
           <li>Browse the list of drop-off locations.</li>
           <li>Search by address or keyword.</li>
           <li>Filter your results by type of donation or material.</li>
         </ul>
       </div>
-      <h2 class="title is-3">
-        Accepting donations or recyclables?
-      </h2>
-      <p>To learn about adding your organization to the finder, contact <a href="mailto:RecyclingOffice@phila.gov">RecyclingOffice@phila.gov</a>.</p>
+
+      <div class="half-data-section">
+        <h2 class="title is-3">
+          Accepting donations or recyclables?
+        </h2>
+      </div>
+
+      <div class="half-data-section">
+        <p>To learn about adding your organization to the finder, contact <a href="mailto:RecyclingOffice@phila.gov">RecyclingOffice@phila.gov</a>.</p>
+      </div>
+
     </div>
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'CustomGreeting',
-  props: {
-    'message': {
-      type: String,
-      default: function() {
-        return 'defaultMessage';
-      },
-    },
-  },
-  data() {
-    let data = {
-      sections: {},
-      subsections: {},
-    };
-    return data;
-  },
-  computed: {
-    i18nEnabled() {
-      if (this.$config.i18n) {
-        return true;
-      }
-      return false;
-
-    },
-    calloutOptions() {
-      return {};
-    },
-    calloutSlots() {
-      return {
-        text: 'test',
-      };
-    },
-    database() {
-      if (this.$store.state.sources[this.$appType].data) {
-        return this.$store.state.sources[this.$appType].data.rows || this.$store.state.sources[this.$appType].data.features || this.$store.state.sources[this.$appType].data;
-      }
-      return [];
-    },
-    hasError() {
-      return this.$store.state.geocode.status === 'error';
-    },
-    errorMessage() {
-      const input = this.$store.state.geocode.input;
-      return `
-          <p>
-            We couldn't find
-            ${input ? '<strong>' + input + '</strong>' : 'that address'}.
-            Are you sure everything was spelled correctly?
-          </p>
-          <p>
-            Here are some examples of things you can search for:
-          </p>
-          <ul>
-            <li>1234 Market St</li>
-            <li>1001 Pine Street #201</li>
-            <li>12th & Market</li>
-            <li>883309050 (an OPA number with no hyphens or other characters)</li>
-          </ul>
-        `;
-    },
-  },
-};
-</script>
-
 <style lang="scss" scoped>
 
-@import "../../node_modules/@phila/pinboard/src/assets/scss/customGreeting.scss";
+.app-list {
+  margin-bottom: 2rem;
+}
 
-  .section {
-    padding: 1.5rem;
-  }
+.main-greeting {
+  padding: 1rem;
+}
 
-  .app-list {
-    margin-bottom: 2rem;
-  }
+.greeting-button {
+  background-color: #0f4d90 !important;
+  border-color: #0f4d90 !important;
+  font-size: 1rem;
+  color: white;
+  cursor: pointer;
+}
 
-  // .open-list-button {
-  //   text-transform: uppercase;
-  //   background-color: #0f4d90;
-  //   color: #ffffff;
-  //   padding-left: 32px;
-  //   padding-right: 32px;
-  //   padding-top: 17px;
-  //   padding-bottom: 17px;
-  // }
+.greeting-button:hover {
+  border-color: #2176d2 !important;
+}
+
+.half-data-section {
+  margin-bottom: 1rem;
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,12 +5,10 @@
 // (we might not need to use axios with new vue async tools)
 // if that is not needed, we can move this info to main.js
 
-// turn off console logging in production
-if (process.env.NODE_ENV === 'production') {
-  console.log = console.info = console.debug = console.error = function () {};
+import isMac from './util/is-mac';
+if (isMac()) {
+  import('./assets/mac-style.scss')
 }
-console.log('main.js process.env.NODE_ENV:', process.env.NODE_ENV, 'process.env.VUE_APP_PUBLICPATH:', process.env.VUE_APP_PUBLICPATH);
-
 
 // Font Awesome Icons
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -22,22 +20,30 @@ import { faMinus as farMinus } from '@fortawesome/pro-regular-svg-icons/faMinus'
 
 library.add(farAngleDown, farAngleUp, farTimes, farPlus, farMinus);
 
+import { markRaw } from 'vue';
 
-// import pinboard
-import pinboard from '@phila/pinboard/src/main.js';
+// import pinboard from '@phila/pinboard';
+import pinboard from '../node_modules/@phila/pinboard/src/main.js';
+
+import '../node_modules/@phila/pinboard/dist/style.css';
 
 // data-sources
 import recycling from './data-sources/recycling';
 
 import customGreeting from './components/customGreeting.vue';
-const customComps = {
+const customComps = markRaw({
   'customGreeting': customGreeting,
-};
+});
 
-import '@creativebulma/bulma-tooltip/dist/bulma-tooltip.min.css';
+// import '@creativebulma/bulma-tooltip/dist/bulma-tooltip.min.css';
 
-pinboard({
-  publicPath: process.env.VUE_APP_PUBLICPATH,
+let $config = {
+  publicPath: import.meta.env.VITE_PUBLICPATH,
+  i18n: {
+    enabled: false,
+    selectorHidden: true,
+    data: {},
+  },
   app: {
     title: 'Resources for recycling and donation',
     subtitle: 'Find out where to donate items or recycle in Philadelphia',
@@ -62,7 +68,8 @@ pinboard({
     },
   },
   locationInfo: {
-    siteName: 'organization_name',
+    siteNameField: 'organization_name',
+    siteName: function(item) { return item.properties.organization_name },
   },
   customComps,
   hiddenRefine: {
@@ -73,7 +80,9 @@ pinboard({
   refine: {
     type: 'categoryField_array',
     value: function(item) {
-      return item.services_offered;
+      if (item && item.properties) {
+        return item.properties.services_offered;
+      }
     },
   },
   alerts: {
@@ -85,14 +94,24 @@ pinboard({
     // },
   },
   markerType: 'circle-marker',
-  circleMarkers:{
-    // color: '#2176d2',
-    borderColor: 'white',
-    weight: 1,
-    radius: 8,
-    mobileRadius: 12,
-    size: 16,
-    mobileSize: 20,
+  // circleMarkers: {
+  //   borderColor: 'white',
+  //   weight: 1,
+  //   radius: 8,
+  //   mobileRadius: 12,
+  //   size: 16,
+  //   mobileSize: 20,
+  // },
+  mapLayer: {
+    id: 'resources',
+    source: 'resources',
+    type: 'circle',
+    paint: {
+      'circle-radius': 7,
+      'circle-color': '#9400c6',
+      'circle-stroke-width': 1,
+      'circle-stroke-color': 'white',
+    },
   },
   cyclomedia: {
     enabled: false,
@@ -105,19 +124,6 @@ pinboard({
   },
   dataSources: {
     recycling,
-  },
-  router: {
-    enabled: false,
-  },
-  geocoder: {
-    url(input) {
-      const inputEncoded = encodeURIComponent(input);
-      return `//api.phila.gov/ais/v1/search/${inputEncoded}`;
-    },
-    params: {
-      // gatekeeperKey: process.env.VUE_APP_GATEKEEPER_KEY,
-      include_units: true,
-    },
   },
   footer: [
     {
@@ -171,105 +177,7 @@ pinboard({
     // </div>\
     // },
   },
-  map: {
-    type: 'mapbox',
-    containerClass: 'map-container',
-    defaultBasemap: 'pwd',
-    center: [ -75.163471, 39.953338 ],
-    zoom: 12,
-    geocodeZoom: 15,
-    imagery: {
-      enabled: false,
-    },
-    basemaps: {
-      pwd: {
-        url: 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer',
-        tiledLayers: [
-          'cityBasemapLabels',
-        ],
-        type: 'featuremap',
-        attribution: 'Parcels: Philadelphia Water',
-      },
-    },
-    tiledLayers: {
-      cityBasemapLabels: {
-        url: 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer',
-        zIndex: '3',
-      },
-    },
-  },
-  // mbStyle: 'mapbox://styles/mapbox/streets-v11',
-  mbStyle: {
-    version: 8,
-    sources: {
-      pwd: {
-        tiles: [
-          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer/tile/{z}/{y}/{x}',
-        ],
-        type: 'raster',
-        tileSize: 256,
-      },
-    },
-    layers: [
-      {
-        id: 'pwd',
-        type: 'raster',
-        source: 'pwd',
-      },
-    ],
-  },
-  basemapSources: {
-    pwd: {
-      source: {
-        tiles: [
-          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer/tile/{z}/{y}/{x}',
-          // '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}'
-        ],
-        type: 'raster',
-        tileSize: 256,
-      },
-      layer: {
-        id: 'pwd',
-        type: 'raster',
-      },
-    },
-    imagery2019: {
-      source: {
-        tiles: [
-          'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2019_3in/MapServer/tile/{z}/{y}/{x}',
-          // '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}'
-        ],
-        type: 'raster',
-        tileSize: 256,
-      },
-      layer: {
-        id: 'imagery2019',
-        type: 'raster',
-      },
-    },
-  },
-  basemapLabelSources:{
-    cityBasemapLabels: {
-      source: {
-        tiles: [ 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer/tile/{z}/{y}/{x}' ],
-        type: 'raster',
-        tileSize: 256,
-      },
-      layer: {
-        id: 'cityBasemapLabels',
-        type: 'raster',
-      },
-    },
-    imageryBasemapLabels: {
-      source: {
-        tiles: [ 'https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_Labels/MapServer/tile/{z}/{y}/{x}' ],
-        type: 'raster',
-        tileSize: 256,
-      },
-      layer: {
-        id: 'imageryBasemapLabels',
-        type: 'raster',
-      },
-    },
-  },
-});
+};
+
+pinboard($config);
+export default $config;
